adc1a6be6112521aec314ad0e37305bccc531488
issa23i <isabelpastorlopezcv@gmail.com>
2023-09-13 18:25:39 +0200

    hashcode y equals

--- src/com/isabelpastorlopezcv/Main.java
+++ src/com/isabelpastorlopezcv/Main.java
@@ -1,5 +1,6 @@
 package com.isabelpastorlopezcv;
 
+import com.isabelpastorlopezcv.entidades.Alumno;
 import com.isabelpastorlopezcv.entidades.Docente;
 import com.isabelpastorlopezcv.entidades.Entidad;
 import com.isabelpastorlopezcv.services.EntidadService;
@@ -23,6 +24,25 @@ public class Main {
         // refactoriing
         // ctrl w w para seleccionar la frase
         // ctrl alt v reemplaza por una variable
+
+        Alumno pepe = new Alumno("Jose","Canton");
+        Alumno pepe2 = new Alumno("Jose","Canton");
+        pepe.setEdad(44);
+
+        Alumno andonio = new Alumno("Antonio", "Mujica");
+        andonio.setApellido2("Encala");
+
+        System.out.println(andonio.getApellido1());
+        System.out.println(andonio.equals(pepe));
+        System.out.println(pepe.equals(pepe2));
+        pepe2.setEdad(50);
+        System.out.println(pepe.equals(pepe2));
+        System.out.println(pepe + " " + pepe2);
+        System.out.println(pepe.hashCode() + "-" + pepe2.hashCode());
+        pepe2.setApellido1("Calatrava");
+        System.out.println(pepe.equals(pepe2));
+        System.out.println(pepe + " " + pepe2);
+        System.out.println(pepe.hashCode() + "-" + pepe2.hashCode());
     }
 
     private static void extracted(String mensaje) {
--- src/com/isabelpastorlopezcv/entidades/Alumno.java
+++ src/com/isabelpastorlopezcv/entidades/Alumno.java
@@ -1,4 +1,73 @@
 package com.isabelpastorlopezcv.entidades;
 
+import java.util.Objects;
+
 public class Alumno {
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+    private int edad;
+
+    public Alumno(String nombre, String apellido1, String apellido2, int edad) {
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+        this.edad = edad;
+    }
+
+    public Alumno(String nombre, String apellido1, String apellido2) {
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+    }
+
+    public Alumno(String nombre, String apellido1) {
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Alumno)) return false;
+        Alumno alumno = (Alumno) o;
+        return getNombre().equals(alumno.getNombre()) && getApellido1().equals(alumno.getApellido1()) && Objects.equals(getApellido2(), alumno.getApellido2());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNombre(), getApellido1(), getApellido2());
+    }
 }
